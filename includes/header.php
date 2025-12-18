fc6ca045eac1bb155810a9b81fdcbea0a631b049
dimas <adimas2@ucol.mx>
2025-12-18 13:05:32 -0600

    Cambios en el diseño de nuestro header..

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="./assets/header.css">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 <header class="header-full">
     <div class="header-container">
 
@@ -8,15 +9,24 @@
             <span>Villas Eureka</span>
         </div>
 
-        <!-- NAV -->
-        <nav class="header-nav">
-            <a href="index.php">Inicio</a>
-            <a href="renta.php">Propiedades en renta</a>
-            <a href="venta.php">Propiedades en venta</a>
-            <a href="villas.php">Nuestras villas</a>
-            <a href="alrededores.php">Alrededores</a>
-            <a href="contacto.php">Contáctanos</a>
-        </nav>
+        <?php 
+  //NECESITAMOS OBTENER EL ARCHIVO EN EL QUE ESTAMOS PARA DIBUJAR NUESTRO ENLACE CON CSS
+  $current_page = basename($_SERVER['PHP_SELF']); 
+?>
+
+<nav class="header-nav">
+    <a href="index.php" class="<?php echo ($current_page == 'index.php') ? 'active' : ''; ?>">Inicio</a>
+    
+    <a href="renta.php" class="<?php echo ($current_page == 'renta.php') ? 'active' : ''; ?>">Propiedades en renta</a>
+    
+    <a href="venta.php" class="<?php echo ($current_page == 'venta.php') ? 'active' : ''; ?>">Propiedades en venta</a>
+    
+    <a href="villas.php" class="<?php echo ($current_page == 'villas.php') ? 'active' : ''; ?>">Nuestras villas</a>
+    
+    <a href="alrededores.php" class="<?php echo ($current_page == 'alrededores.php') ? 'active' : ''; ?>">Alrededores</a>
+    
+    <a href="contacto.php" class="<?php echo ($current_page == 'contacto.php') ? 'active' : ''; ?>">Contáctanos</a>
+</nav>
 
         <!-- ACCIONES -->
         <div class="header-actions">
@@ -25,7 +35,14 @@
         </div>
 
     </div>
-
-    <!-- Línea decorativa -->
-    <div class="header-line"></div>
+    <script>
+        window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header-full');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+    </script>
 </header>
